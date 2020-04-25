8b0e11606773eb3d0c0a676115dbdfe17a10aad7
Dipanshu Chaubey <dipanshuchaubey@gmail.com>
2020-04-25 22:09:29 +0530

    TEST - Router Tests

--- controllers/__test__/auth.test.js
+++
@@ -1,3 +0,0 @@
-test('should login user', () => {
-  expect({ email: 'dipanshu@gmail.com', password: '123456' }).toEqual();
-});
--- test/routes.test.js
+++ test/routes.test.js
@@ -1,26 +1,52 @@
-const Bootcamp = require('../models/Bootcamp');
-const { MongoClient } = require('mongodb');
+const axios = require('axios');
 
-describe('insert', () => {
-  let connection;
-  let db;
+test('should container bootcamp', async () => {
+  expect.assertions(1);
+  const data = await axios.get(
+    'https://bootcamper.ml/api/v1/bootcamps/5d713a66ec8f2b88b8f830b8'
+  );
 
-  beforeAll(async () => {
-    connection = await MongoClient.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true
-    });
-    db = await connection.db(global.bootcamp);
-  });
+  expect(data.data.data.name).toEqual('ModernTech Bootcamp');
+});
 
-  afterAll(async () => {
-    await connection.close();
-    await db.close();
-  });
+test('should contain a course', async () => {
+  expect.assertions(1);
 
-  test('Should fetch all bootcamps', async () => {
-    const bootcamps = await Bootcamp.findOne({
-      _id: '5dc6c20bd241963cc4ee151d'
-    });
-    expect(bootcamps.name).toEqual('Devworks Bootcamp');
-  });
+  const data = await axios.get(
+    'https://bootcamper.ml/api/v1/bootcamps/5d713a66ec8f2b88b8f830b8/courses/5d725cd2c4ded7bcb480eaa2'
+  );
+
+  expect(data.data.data.title).toEqual('UI/UX');
+});
+
+/**
+ * THESE ARE ALL RESTRICTED REQUESTS FOR NON AUTHENTICATED USERS
+ */
+
+test('Get current user should be restriced', async () => {
+  expect.assertions(1);
+
+  let status;
+
+  try {
+    await axios.get('https://bootcamper.ml/api/v1/auth/me');
+  } catch (err) {
+    status = err.response.status;
+  }
+
+  expect(status).toBe(401);
+});
+
+test('Register new user with NOT UNIQUE email', () => {
+  expect.assertions(1);
+
+  return axios
+    .post('https://bootcamper.ml/api/v1/auth/register', {
+      name: 'Rick Mick',
+      email: 'rick@gmail.com',
+      password: '123456',
+      role: 'publisher'
+    })
+    .then(data => console.log(data))
+    .catch(err => expect(err.response.status).toBe(400));
 });
